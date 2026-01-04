0cddcded156127705d8905ee62ab023c03192c1f
John Bochicchio <jpbochicchio@gmail.com>
2026-01-04 16:03:39 -0500

    Add working hex drawing and start extracting TOI assets

--- src/Root.ts
+++ src/Root.ts
@@ -4,40 +4,17 @@ import {
   useChild,
   Canvas,
   Vector,
-  SystemFont,
   Label,
+  SystemFont,
   useDraw,
-} from '@hex-engine/2d';
-import HexagonalGrid from './logic/entity/hex/HexagonalGrid';
+} from "@hex-engine/2d";
+import HexGrid from "./logic/entity/hex/HexGrid";
 
 export default function Root() {
   useType(Root);
 
-  const canvas = useNewComponent(() => Canvas({ backgroundColor: 'white' }));
+  const canvas = useNewComponent(() => Canvas({ backgroundColor: "white" }));
   canvas.fullscreen();
 
-  useChild(() => HexagonalGrid(new Vector(20, 50)));
-
-  useChild(() => {
-    const font = useNewComponent(() =>
-      SystemFont({ name: 'sans-serif', size: 16, color: 'black' }),
-    );
-
-    const label = useNewComponent(() =>
-      Label({
-        text: 'Left click cell for blue, right for purple, middle for white',
-        font: font,
-      }),
-    );
-
-    useDraw((context) => {
-      label.draw(context, { x: 3, y: font.size});
-    });
-  });
-
-  const canvasCenter = new Vector(
-    canvas.element.width / 2,
-    canvas.element.height / 2,
-  );
+  useChild(() => HexGrid(new Vector(40, 60), 10, 10));
 }
-
