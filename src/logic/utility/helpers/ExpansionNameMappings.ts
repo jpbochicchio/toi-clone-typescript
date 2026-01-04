2b0e3921ee79f1a6ac867f6b6753f9ef2cb76c18
John Bochicchio <jpbochicchio@gmail.com>
2026-01-04 16:30:45 -0500

    Convert map to readonly

--- src/logic/utility/helpers/ExpansionNameMappings.ts
+++ src/logic/utility/helpers/ExpansionNameMappings.ts
@@ -1,6 +1,6 @@
 import { ExpansionName } from "../enumerations/ExpansionName";
 
-export const ExpansionObstacleMap: Map<ExpansionName, string> = new Map<ExpansionName, string>([
+export const ExpansionObstacleMap: ReadonlyMap<ExpansionName, string> = new Map<ExpansionName, string>([
     [ExpansionName.BASE, "base"],
     [ExpansionName.DAYS_OF_THE_FOX, "desert"],
     [ExpansionName.FURY_OF_THE_BEAR, "winter"],
